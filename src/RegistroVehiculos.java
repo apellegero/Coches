8a36a36b89b9a88370a518e4bc2000c265f4a047
apellegero <albertpeye@gmail.com>
2016-10-19 15:57:24 +0200

    practica finalitzada

--- src/RegistroVehiculos.java
+++ src/RegistroVehiculos.java
@@ -8,22 +8,29 @@
         private Set<Coche> coches = new HashSet<>();
 
         public void registrarVehiculo(Coche coche){
+            coches.add(coche);
         }
 
         // Optional<Coche> es una nueva funcionalidad de Java 8 para evitar trabajar con null
         // Lo puedes omitir si trabajas con Java 7
         public Optional<Coche> obtenerVehiculo(String matricula){
+            return coches.stream().filter(coche -> coche.getMatricula().equals(matricula)).findFirst();
         }
 
         public void eliminarVehiculo(String matricula){
+            coches.removeIf(coche -> coche.getMatricula().equals(matricula));
         }
 
         public Optional<Coche> obtenerVehiculoPrecioMax(){
+            return coches.stream().max(Comparator.comparing(Coche::getPrecio));
+
         }
 
         public List<Coche> obtenerVehiculosMarca(String marca){
+            return  coches.stream().filter(coche -> coche.getMarca().equals(marca)).collect(Collectors.toList());
         }
 
         public List<Coche> obtenerTodos(){
+            return coches.stream().collect(Collectors.toList());
         }
 }
